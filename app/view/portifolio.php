628f5d2fdff3644ee5a285be2c4b855dd65fd837
Matheus Santos Da Fonseca <matheus.fonseca@picpay.com>
2022-08-13 18:16:00 -0300

    criação Portifólio

--- app/view/portifolio.php
+++ app/view/portifolio.php
@@ -5,19 +5,37 @@
       <h3>Design</h3>
       <div class="contreinnerDesign">
         <?php
-        $titulosPortifolio = ['Design para o Dia das Mães', 'Design feito de Estudo', 'Design para Hamburgueria'];
+        $titulosDesign = ['Design para o Dia das<br>Mães', 'Design feito de Estudo', 'Design para<br>Hamburgueria'];
         for ($i = 0; $i <= 2; $i++) {
         ?>
           <div class="projeto">
             <figure>
-              <img src="/www.matheussantosdev.com/app/assets/imgs/designs/design_<?php echo $i; ?>.jpg" alt="<?php echo $titulosPortifolio[$i]; ?>" />
+              <img src="/www.matheussantosdev.com/app/assets/imgs/designs/design_<?php echo $i; ?>.jpg" alt="<?php echo $titulosDesign[$i]; ?>" />
             </figure>
-            <p><?php echo $titulosPortifolio[$i]; ?></p>
+            <p><?php echo $titulosDesign[$i]; ?></p>
           </div>
         <?php
         }
         ?>
       </div>
+      <h3>Programação</h3>
+      <div class="contreinnerProgramacao">
+        <?php
+        $titulosProgramacao = ['Regra de 3'];
+        for ($i = 0; $i < 1; $i++) {
+        ?><a href="http://localhost/www.matheussantosdev.com/portifolio/Regra_de_3">
+            <div class="projeto">
+              <figure>
+                <img src="/www.matheussantosdev.com/app/assets/imgs/programacao/<?php echo $i; ?>.png" alt="<?php echo $titulosProgramacao[$i]; ?>" />
+              </figure>
+              <p><?php echo $titulosProgramacao[$i]; ?></p>
+              <p style="font-size:1rem; margin-top:5px;">Clique Aqui !</p>
+          </a>
+      </div>
+    <?php
+        }
+    ?>
+    </div>
     </div>
   </section>
 </main>
