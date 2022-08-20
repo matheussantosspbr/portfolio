571be0d457a6b4ce4388115df328960a020a4433
Matheus Santos Da Fonseca <matheus.fonseca@picpay.com>
2022-08-20 18:55:40 -0300

    manutenção

--- autoload.php
+++ autoload.php
@@ -1,10 +1,10 @@
 <?php
 spl_autoload_register(function ($nome_arquivo) {
-  if (file_exists('app/controller/' . $nome_arquivo . '.php')) {
-    require 'app/controller/' . $nome_arquivo . '.php';
-  } elseif (file_exists('app/model/' . $nome_arquivo . '.php')) {
-    require 'app/model/' . $nome_arquivo . '.php';
-  } elseif (file_exists('app/core/' . $nome_arquivo . '.php')) {
-    require 'app/core/' . $nome_arquivo . '.php';
+  if (file_exists(__DIR__ . 'app/controller/' . $nome_arquivo . '.php')) {
+    require __DIR__ . 'app/controller/' . $nome_arquivo . '.php';
+  } elseif (file_exists(__DIR__ . 'app/model/' . $nome_arquivo . '.php')) {
+    require __DIR__ . 'app/model/' . $nome_arquivo . '.php';
+  } elseif (file_exists(__DIR__ . 'app/core/' . $nome_arquivo . '.php')) {
+    require __DIR__ . 'app/core/' . $nome_arquivo . '.php';
   }
 });
