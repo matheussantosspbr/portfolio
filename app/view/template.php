287468bd51f91d215265917a050334c9b071a849
Matheus Santos Da Fonseca <matheus.fonseca@picpay.com>
2022-08-20 19:10:46 -0300

    manutenção

--- app/view/template.php
+++ app/view/template.php
@@ -5,12 +5,11 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="shortcut icon" href="logo.png" />
   <meta name="theme-color" content="#0b0f13" />
   <meta name="apple-mobile-web-app-status-bar-style" content="#0b0f13" />
   <meta name="msapplication-navbutton-color" content="#0b0f13" />
-  <link rel="shortcut icon" href="../assets/logo/logo.png" />
-  <link rel="stylesheet" href="../assets/base/style.css">
+  <link rel="shortcut icon" href=<?php echo __DIR__ . "/../assets/logo/logo.png" ?> />
+  <link rel="stylesheet" href=<?php echo __DIR__ . "/../assets/base/style.css" ?>>
   <?php
   $this->carregarHeader();
   $this->carregarViewNoTemplate($nomeView, $dadosModel);
