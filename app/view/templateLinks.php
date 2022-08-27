db3dd5bdff7828170172af243ea75d30330f102c
Matheus Santos Da Fonseca <matheus.fonseca@picpay.com>
2022-08-27 12:33:15 -0300

    Implemantação da pagina de Links

--- app/view/templateLinks.php
+++ app/view/templateLinks.php
@@ -56,7 +56,7 @@ $caminhoStyle = __DIR__ . '/../../assets/style/' . $style . '.css';
   <meta name="msapplication-navbutton-color" content="#0b0f13" />
   <link rel="shortcut icon" href=<?php echo $caminhoLogo; ?> />
   <link rel="stylesheet" href=<?php echo $caminhoStyle; ?>>
-  <title></title>
+  <title>Links</title>
 </head>
 
 <body>
@@ -64,7 +64,6 @@ $caminhoStyle = __DIR__ . '/../../assets/style/' . $style . '.css';
   $this->carregarViewNoTemplate($nomeView, $dadosModel);
   ?>
   <script src=<?php echo $caminhoScript; ?>></script>
-  <script src=<?php echo $caminhoScriptBase ?>></script>
 </body>
 
 </html>
