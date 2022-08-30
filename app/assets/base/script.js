e05a55d8cf33c4542a95f1dd14b31462c840b126
Matheus Santos Da Fonseca <matheus.fonseca@picpay.com>
2022-08-29 22:24:47 -0300

    Implatação de Animação ao abrir o menu

--- app/assets/base/script.js
+++ app/assets/base/script.js
@@ -2,8 +2,8 @@ let open = document.querySelector('.open')
 let close = document.querySelector('.close')
 
 open.addEventListener('click', function () {
-  document.querySelector('nav').classList.add('show')
   animation()
+  document.querySelector('nav').classList.add('show')
 })
 
 close.addEventListener('click', function () {
