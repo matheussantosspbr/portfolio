96175580a099a009890f8edad0bb8fea9a0a3800
Matheus Santos Da Fonseca <matheus.fonseca@picpay.com>
2022-10-10 21:51:49 -0300

    Implementeção da melhoria

--- app/assets/base/script.js
+++ app/assets/base/script.js
@@ -9,33 +9,3 @@ open.addEventListener('click', function () {
 close.addEventListener('click', function () {
   document.querySelector('nav').classList.remove('show')
 })
-
-/* ============ Atualização da minha idade altomaticamente =============  */
-let pIdade = document.querySelector('#idade')
-
-function calculaIdade(dataNasc) {
-  var dataAtual = new Date()
-  var anoAtual = dataAtual.getFullYear()
-  var anoNascParts = dataNasc.split('/')
-  var diaNasc = anoNascParts[0]
-  var mesNasc = anoNascParts[1]
-  var anoNasc = anoNascParts[2]
-  var idade = anoAtual - anoNasc
-  var mesAtual = dataAtual.getMonth() + 1
-  //Se mes atual for menor que o nascimento, nao fez aniversario ainda;
-  if (mesAtual < mesNasc) {
-    idade--
-  } else {
-    //Se estiver no mes do nascimento, verificar o dia
-    if (mesAtual == mesNasc) {
-      if (new Date().getDate() < diaNasc) {
-        //Se a data atual for menor que o dia de nascimento ele ainda nao fez aniversario
-        idade--
-      }
-    }
-  }
-  return idade
-}
-let idade = calculaIdade('03/09/2004')
-
-pIdade.innerHTML = idade
